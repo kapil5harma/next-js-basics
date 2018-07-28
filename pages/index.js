c9fc2825b2ec98901b3d01f42b7440fdab98d6bb
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-07-28 17:53:38 +0530

    Understanding the Basics

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
+import Link from 'next/link';
+import Router from 'next/router';
+
 const indexPage = () => {
   return (
     <div>
       <h1>The Main Page</h1>
+      <p>
+        Go to{' '}
+        <Link href="/auth">
+          <a>Auth</a>
+        </Link>{' '}
+        (using next/link)
+      </p>
+      <button onClick={() => Router.push('/auth')}>
+        Go to Auth (using next/router)
+      </button>
     </div>
   );
 };
