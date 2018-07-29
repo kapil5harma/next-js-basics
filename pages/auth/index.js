8b2f16db2e9b21941514b67e991f23b230612d61
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-07-29 20:33:54 +0530

    Components in next.js

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import User from '../../components/User';
 
 const authIndexPage = () => {
   return (
     <div>
       <h1>The Auth Index Page</h1>
+      <User name="Kapil Sharma" age={24} />
     </div>
   );
 };
