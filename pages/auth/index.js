e75ac52c1959b5e911cbb3ea99c4e1366df45d0b
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-07-29 21:09:00 +0530

    getInitialProps - special lifecycle hook exclusive to next.js

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import User from '../../components/User';
 
-const authIndexPage = () => {
+const authIndexPage = props => {
   return (
     <div>
-      <h1>The Auth Index Page</h1>
+      <h1>The Auth Index Page - {props.appName}</h1>
       <User name="Kapil Sharma" age={24} />
     </div>
   );
+};
+
+authIndexPage.getInitialProps = context => {
+  // console.log('context: ', context);
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ appName: 'Super App (Auth)' });
+    }, 2000);
+  });
+  return promise;
 };
 
 export default authIndexPage;
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Link from 'next/link';
 import Router from 'next/router';
 
-const indexPage = () => {
-  return (
-    <div>
-      <h1>The Main Page</h1>
-      <p>
-        Go to{' '}
-        <Link href="/auth">
-          <a>Auth</a>
-        </Link>{' '}
-        (using next/link)
-      </p>
-      <button onClick={() => Router.push('/auth')}>
-        Go to Auth (using next/router)
-      </button>
-    </div>
-  );
-};
+class IndexPage extends Component {
+  static getInitialProps(context) {
+    // console.log('context: ', context);
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({ appName: 'Super App' });
+      }, 2000);
+    });
+    return promise;
+  }
 
-export default indexPage;
+  render() {
+    return (
+      <div>
+        <h1>The Main Page of {this.props.appName}</h1>
+        <p>
+          Go to{' '}
+          <Link href="/auth">
+            <a>Auth</a>
+          </Link>{' '}
+          (using next/link)
+        </p>
+        <button onClick={() => Router.push('/auth')}>
+          Go to Auth (using next/router)
+        </button>
+      </div>
+    );
+  }
+}
+
+export default IndexPage;
